aba3f6b90d6ba68885f78d10df0b75a136376023
Yanhan Nie <ynie@hawaiirg.com>
2015-04-27 09:55:43 -1000

    check resize handle exists and only insert it if not

--- src/jquery.gridster.js
+++ src/jquery.gridster.js
@@ -430,7 +430,9 @@
     */
     fn.add_resize_handle = function($w) {
         var append_to = this.options.resize.handle_append_to;
-        $(this.resize_handle_tpl).appendTo( append_to ? $(append_to, $w) : $w);
+        var handle = $('.' + this.options.resize_handle_class, $w);
+        if(handle.length == 0)
+            $(this.resize_handle_tpl).appendTo( append_to ? $(append_to, $w) : $w);
 
         return this;
     };
